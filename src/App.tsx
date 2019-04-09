108ba198bf4f5f21dd3be1402fccad1db0238b70
justinaus <justinaus2k@gmail.com>
2019-04-09 14:50:47 +0900

    Add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
+import { Button, ButtonToolbar } from 'react-bootstrap';
+
 class App extends Component {
   render() {
     return (
       <div>
-        hello cm2
+        <ButtonToolbar>
+          <Button variant="primary">Primary</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="success">Success</Button>
+          <Button variant="warning">Warning</Button>
+          <Button variant="danger">Danger</Button>
+          <Button variant="info">Info</Button>
+          <Button variant="light">Light</Button>
+          <Button variant="dark">Dark</Button>
+          <Button variant="link">Link</Button>
+        </ButtonToolbar>;
       </div>
     );
   }
