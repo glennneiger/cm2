313dff7d49a16ca290d86b2326968e9e69feb596
justinaus <justinaus2k@gmail.com>
2019-04-16 18:34:09 +0900

    Update channel list by fetch

--- src/components/ChannelList.tsx
+++ src/components/ChannelList.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Table, Button } from 'react-bootstrap';
-import './ChannelList.css';
 import { RouteComponentProps } from 'react-router-dom';
+import './ChannelList.css';
 
 interface IChannelStatus {
   id: Number,
@@ -31,51 +31,44 @@ class ChannelList extends Component<RouteComponentProps, IState> {
     .then( ( response ) => {
       return response.json();
     } ).then( ( data ) => {
-      
-
       this.setState( {
         channelStatusList: data
       } );
-
-      console.log( this.state.channelStatusList );
     } ).catch( ( error ) => {
       console.log( error );
     } );
   }
+
+  onClickItem = ( channelStatus: IChannelStatus ) => {
+    console.log( channelStatus );
+  }
+
+  renderChannelItem = ( channelStatus: IChannelStatus ) => {
+    return (
+      <tr key={ String( channelStatus.id ) }>
+        <td>{ channelStatus.title }</td>
+        <td>{ channelStatus.traffic }</td>
+        <td><Button variant="primary" onClick={ () => this.onClickItem( channelStatus ) }>입장</Button></td>
+      </tr>
+    );
+  }
   
   render() {
+    const channelStatusList = this.state.channelStatusList;
+
     return (
       <Table hover size="sm">
-  <thead>
-    <tr>
-      <th>채널</th>
-      <th>혼잡도</th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>고수</td>
-      <td>혼잡</td>
-      <td><Button variant="primary">입장</Button></td>
-    </tr>
-    <tr>
-      <td>중수</td>
-      <td>원할</td>
-      <td><Button variant="primary">입장</Button></td>
-    </tr>
-    <tr>
-      <td>하수</td>
-      <td>점검</td>
-      <td><Button variant="primary">입장</Button></td>
-    </tr>
-    <tr>
-      <td>뉴비</td>
-      <td>점검</td>
-      <td><Button variant="primary">입장</Button></td>
-    </tr>
-  </tbody>
-</Table>
+        <thead>
+          <tr>
+            <th>채널</th>
+            <th>혼잡도</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          { channelStatusList.map( this.renderChannelItem ) }
+        </tbody>
+      </Table>
     );
   }
 }
