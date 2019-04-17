846312560bc3990ec56f199a58e4d2c87cbc6465
justinaus <justinaus2k@gmail.com>
2019-04-17 18:57:01 +0900

    Update hello socket

--- client/src/components/lobby/ChatRoom.tsx
+++ client/src/components/lobby/ChatRoom.tsx
@@ -1,10 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component, ComponentProps } from 'react';
+// import socketIOClient from "socket.io-client";
+// import * as socketIOClient from "socket.io-client";
+import * as io from 'socket.io-client';
 
-class ChatRoom extends Component {
+interface IState {
+  color: Number
+}
+
+class ChatRoom extends Component< any, IState > {
+  // endpoint: String = 'localhost:4001';
+
+  socket = io.connect( 'localhost:4001' )
+
+  constructor( props: any ) {
+    super( props );
+
+    this.state = {
+      color: 0xffffff
+    }
+
+    // const socket = io.connect( 'localhost:4001' )
+
+  }
+
+  componentDidMount() {
+    console.log( 'did mount' );
+
+    this.socket.on('change color', (col: any) => {
+      console.log( "GOT COLOR" );
+      console.log( col );
+      document.body.style.backgroundColor = col
+  })
+  }
+
+  sendColor = ( color: any ) => {
+    this.socket.emit('change color', color )
+  }
+
   render() {
     return (
       <>
         chatroom
+        <button onClick={ () => this.sendColor( '#0000ff' ) }>blue</button>
+        <button onClick={ () => this.sendColor( '#ff0000' ) }>red</button>
       </>
     );
   }
